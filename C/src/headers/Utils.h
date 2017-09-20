82198ae765c3c27862bebd139cf64d87ae55881b
Thomas COUCHOUD <zerderr@gmail.com>
2017-09-20 21:51:12 +0200

    Put back macros on one line

--- C/src/headers/Utils.h
+++ C/src/headers/Utils.h
@@ -8,20 +8,10 @@
 #define UNUSED(x) (void)(x)
 
 #define MALLOC(t, n) (t *) malloc((n) * sizeof(t))
-#define MCHECK(x, m) \
-			if(!(x)) \
-			{ \
-				fprintf(stderr, "MALLOC ERROR IN FUNCTION %s.", m); \
-				exit(EXIT_FAILURE); \
-			}
+#define MCHECK(x, m) if(!(x)){fprintf(stderr, "MALLOC ERROR IN FUNCTION %s.", m);exit(EXIT_FAILURE);}
 
 #define REALLOC(t, p, n) (t *) realloc(p, (n) * sizeof(t))
-#define RCHECK(x, m) \
-			if(!(x)) \
-			{ \
-				fprintf(stderr, "REALLOC ERROR IN FUNCTION %s.", m); \
-				exit(EXIT_FAILURE); \
-			}
+#define RCHECK(x, m) if(!(x)){fprintf(stderr, "REALLOC ERROR IN FUNCTION %s.", m);exit(EXIT_FAILURE);}
 
 /**
  * @param p The variable that will contain the allocated pointer.
