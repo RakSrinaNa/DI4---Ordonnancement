bb0b1f3ee43071e2a14310c2e1e7e6443ecb08d4
Thomas COUCHOUD <zerderr@gmail.com>
2017-09-27 21:04:38 +0200

    Use Bool in unit functions

--- C/src/unit/UnitUtils.c
+++ C/src/unit/UnitUtils.c
@@ -12,18 +12,18 @@ void unit_breakpoint()
 	raise(SIGINT);
 }
 
-int unit_intArrayEquals(const int * base, const int * compare, int length)
+Bool unit_intArrayEquals(const int * base, const int * compare, int length)
 {
 	for(int i = 0; i < length; i++)
 		if(base[i] != compare[i])
-			return 0;
-	return 1;
+			return False;
+	return True;
 }
 
-int unit_uintArrayEquals(const unsigned int * base, const unsigned int * compare, int length)
+Bool unit_uintArrayEquals(const unsigned int * base, const unsigned int * compare, int length)
 {
 	for(int i = 0; i < length; i++)
 		if(base[i] != compare[i])
-			return 0;
-	return 1;
+			return False;
+	return True;
 }
--- C/src/unit/headers/UnitUtils.h
+++ C/src/unit/headers/UnitUtils.h
@@ -5,6 +5,7 @@
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include "../../headers/Utils.h"
 
 /**
  * Add a "breakpoint" and exit the program.
@@ -24,7 +25,7 @@ void unit_breakpoint();
  * @param base The array containing expected values.
  * @param compare The array containing the computed values.
  * @param length The arrays length.
- * @return 1 if the same, 0 else.
+ * @return True if the same, False else.
  */
 int unit_intArrayEquals(const int * base, const int * compare, int length);
 
@@ -33,8 +34,8 @@ int unit_intArrayEquals(const int * base, const int * compare, int length);
  * @param base The array containing expected values.
  * @param compare The array containing the computed values.
  * @param length The arrays length.
- * @return 1 if the same, 0 else.
+ * @return True if the same, False else.
  */
-int unit_uintArrayEquals(const unsigned int * base, const unsigned int * compare, int length);
+Bool unit_uintArrayEquals(const unsigned int * base, const unsigned int * compare, int length);
 
 #endif
