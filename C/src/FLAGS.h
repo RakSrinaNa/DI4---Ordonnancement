f36539f7253fcea1f6f174e84eca5dfef081d155
Thomas COUCHOUD <zerderr@gmail.com>
2017-10-02 19:43:35 +0200

    Add Flags & Files to CMAke

--- C/src/FLAGS.h
+++ C/src/FLAGS.h
@@ -3,4 +3,16 @@
 
 #define DEBUG 1
 
+#define DELTA_NB_BATCH 4
+#define NB_ITERATION_MAX 2000
+#define AVEC_DIVERSIFICATION 1
+#define TAILLE_TABOU 5
+
+#define FLAG_EBSR 1
+#define FLAG_EFSR 1
+#define FLAG_SWAP 1
+#define FLAG_2OPT 0
+#define FLAG_FIRST_IMPROVE 1
+#define FLAG_TABOU_LOGIC 0
+
 #endif //TABOU_FLAGS_H
