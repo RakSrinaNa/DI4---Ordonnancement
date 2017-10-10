1e5d5d2bc20d38deffe969c872803a959763d2ee
Thomas COUCHOUD <zerderr@gmail.com>
2017-10-10 12:23:48 +0200

    Add tests

--- C/src/unit/SolutionUnit.c
+++ C/src/unit/SolutionUnit.c
@@ -155,5 +155,24 @@ void solutionUnit()
 		unit_error("Solution 25: Bad sequence process");
 	free(sol);
 	
+	unsigned int tasks9[] = {0, 2};
+	time = solution_deliveryDelay(instance, 2, tasks9, 100);
+	if(time != 12)
+		unit_error("Solution 26: Bad delivery delay, got %d expected %d", time, 12);
+	
+	time = solution_deliveryDelay(instance, 2, tasks9, 0);
+	if(time != 0)
+		unit_error("Solution 27: Bad delivery delay, got %d expected %d", time, 0);
+	
+	unsigned int tasks10[] = {0, 1};
+	time = solution_deliveryDelay(instance, 2, tasks10, 100);
+	if(time != 11)
+		unit_error("Solution 28: Bad delivery delay, got %d expected %d", time, 11);
+	
+	unsigned int tasks11[] = {2, 0};
+	time = solution_deliveryDelay(instance, 2, tasks11, 100);
+	if(time != 12)
+		unit_error("Solution 28: Bad delivery delay, got %d expected %d", time, 12);
+	
 	instance_destroy(instance);
 }
