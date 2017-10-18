61515ba9bb29bea77d8f3b2c0098c996603a1de7
Thomas COUCHOUD <zerderr@gmail.com>
2017-10-18 10:22:39 +0200

    Fix valgrind

--- C/src/unit/InstanceUnit.c
+++ C/src/unit/InstanceUnit.c
@@ -29,4 +29,6 @@ void instanceUnit() //Mainly test for memory leaks
 	unsigned int correctOrder3[] = {2, 3, 1, 4, 0};
 	if(!unit_uintArrayEquals(correctOrder3, order, 5))
 		unit_error("Instance 1: Bad sort by due date");
+	
+	instance_destroy(instance);
 }
