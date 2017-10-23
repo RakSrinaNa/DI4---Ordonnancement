b962ef2b71c5c756098bf37d34a71c7255a1ba75
Schttopup <clement.0310@free.fr>
2017-10-23 15:57:59 +0200

    sort + cbp

--- C/src/Sort.c
+++ C/src/Sort.c
@@ -6,3 +6,12 @@ Solution * sort_swapDeliveries(Solution *solution, unsigned int task1, unsigned 
 	solution_switchTaskPack(solution, task1, task2);
 	return sol;
 }
+
+
+// TODO EBSR & EFSR separated
+Solution * sort_moveDeliveries(Solution *solution, unsigned int task, unsigned int pack)
+{
+	Solution * sol = solution_copy(solution);
+	solution_moveTaskPack(solution, task, pack);
+	return sol;
+}
--- C/src/headers/Sort.h
+++ C/src/headers/Sort.h
@@ -3,6 +3,12 @@
 
 #include "Solution.h"
 
+// TODO doc
+// TODO unit
 Solution * sort_swapDeliveries(Solution *solution, unsigned int task1, unsigned int task2);
+
+// TODO doc
+// TODO unit
+Solution * sort_moveDeliveries(Solution *solution, unsigned int task, unsigned int pack);
 
 #endif
