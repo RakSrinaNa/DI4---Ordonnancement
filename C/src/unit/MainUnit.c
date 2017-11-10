8db4736c2d5bf9e798fc75587ce52d06cad1574a
Schttopup <clement.0310@free.fr>
2017-11-10 12:13:14 +0100

    ta mere gcc

--- C/src/unit/MainUnit.c
+++ C/src/unit/MainUnit.c
@@ -27,39 +27,39 @@ int mainUnit()
 	printf("UNIT: Executing Task tests...\n");
 	taskUnit();
 	printf("UNIT: OK\n");
-	printf(linebreak);
-	printf(linebreakbreak);
+	printf("%s", linebreak);
+	printf("%s", linebreakbreak);
 	printf("UNIT: Executing Instance tests...\n");
 	instanceUnit();
 	printf("UNIT: OK\n");
-	printf(linebreak);
-	printf(linebreakbreak);
+	printf("%s", linebreak);
+	printf("%s", linebreakbreak);
 	printf("UNIT: Executing Parser tests...\n");
 	parserUnit();
 	printf("UNIT: OK\n");
-	printf(linebreak);
+	printf("%s", linebreak);
 	
-	printf(linebreakbreak);
+	printf("%s", linebreakbreak);
 	printf("UNIT: Executing Pack tests...\n");
 	packUnit();
 	printf("UNIT: OK\n");
-	printf(linebreak);
-	printf(linebreakbreak);
+	printf("%s", linebreak);
+	printf("%s", linebreakbreak);
 	printf("UNIT: Executing SolutionInfo tests...\n");
 	solutionInfoUnit();
 	printf("UNIT: OK\n");
-	printf(linebreak);
-	printf(linebreakbreak);
+	printf("%s", linebreak);
+	printf("%s", linebreakbreak);
 	printf("UNIT: Executing Solution tests...\n");
 	solutionUnit();
 	printf("UNIT: OK\n");
-	printf(linebreak);
+	printf("%s", linebreak);
 	
-	printf(linebreakbreak);
+	printf("%s", linebreakbreak);
 	printf("UNIT: Executing Sequencer tests...\n");
 	sequencerUnit();
 	printf("UNIT: OK\n");
-	printf(linebreak);
+	printf("%s", linebreak);
 	
 	if(UNIT_FAILED)
 		exit(EXIT_FAILURE);
