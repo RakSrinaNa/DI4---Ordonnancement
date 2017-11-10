692d972312377a173c1e3d129451aa84ddb4f43e
Schttopup <clement.0310@free.fr>
2017-11-10 12:09:04 +0100

    opti char*

--- C/src/unit/MainUnit.c
+++ C/src/unit/MainUnit.c
@@ -20,44 +20,46 @@ int mainUnit()
 	srand((unsigned int) time(NULL));
 	setbuf(stdout, NULL);
 	setbuf(stderr, NULL);
+	char *linebreak = "-------------------------------------------------------------------------\n";
+	char *linebreakbreak = "\n-------------------------------------------------------------------------\n";
 
-	printf("\n-------------------------------------------------------------------------\n");
+	printf(linebreakbreak);
 	printf("UNIT: Executing Task tests...\n");
 	taskUnit();
 	printf("UNIT: OK\n");
-	printf("-------------------------------------------------------------------------\n");
-	printf("\n-------------------------------------------------------------------------\n");
+	printf(linebreak);
+	printf(linebreakbreak);
 	printf("UNIT: Executing Instance tests...\n");
 	instanceUnit();
 	printf("UNIT: OK\n");
-	printf("-------------------------------------------------------------------------\n");
-	printf("\n-------------------------------------------------------------------------\n");
+	printf(linebreak);
+	printf(linebreakbreak);
 	printf("UNIT: Executing Parser tests...\n");
 	parserUnit();
 	printf("UNIT: OK\n");
-	printf("-------------------------------------------------------------------------\n");
+	printf(linebreak);
 	
-	printf("\n-------------------------------------------------------------------------\n");
+	printf(linebreakbreak);
 	printf("UNIT: Executing Pack tests...\n");
 	packUnit();
 	printf("UNIT: OK\n");
-	printf("-------------------------------------------------------------------------\n");
-	printf("\n-------------------------------------------------------------------------\n");
+	printf(linebreak);
+	printf(linebreakbreak);
 	printf("UNIT: Executing SolutionInfo tests...\n");
 	solutionInfoUnit();
 	printf("UNIT: OK\n");
-	printf("-------------------------------------------------------------------------\n");
-	printf("\n-------------------------------------------------------------------------\n");
+	printf(linebreak);
+	printf(linebreakbreak);
 	printf("UNIT: Executing Solution tests...\n");
 	solutionUnit();
 	printf("UNIT: OK\n");
-	printf("-------------------------------------------------------------------------\n");
+	printf(linebreak);
 	
-	printf("\n-------------------------------------------------------------------------\n");
+	printf(linebreakbreak);
 	printf("UNIT: Executing Sequencer tests...\n");
 	sequencerUnit();
 	printf("UNIT: OK\n");
-	printf("-------------------------------------------------------------------------\n");
+	printf(linebreak);
 	
 	if(UNIT_FAILED)
 		exit(EXIT_FAILURE);
