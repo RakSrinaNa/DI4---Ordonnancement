95b86833bf40c43ab240f33d8c26e0a7673e6069
Thomas COUCHOUD <zerderr@gmail.com>
2017-11-22 16:45:26 +0100

    TabuList create & destroy

--- C/src/prod/TabuList.c
+++ C/src/prod/TabuList.c
@@ -1,6 +1,24 @@
 #include "headers/TabuList.h"
+#include "headers/Utils.h"
 
 TabuList * tabuList_create(unsigned int maxSize)
 {
+	TabuList * list = NULL;
+	MMALLOC(list, TabuList, 1, "tabuList_create");
+	list->maxSize = maxSize;
+	list->size = 0;
+	list->next = NULL;
+	return list;
+}
 
+void tabuList_destroy(TabuList * list)
+{
+	TabuItem * current = list->next;
+	while(current != NULL)
+	{
+		TabuItem * next = current->next;
+		free(current);
+		current = next;
+	}
+	free(list);
 }
--- C/src/prod/headers/TabuList.h
+++ C/src/prod/headers/TabuList.h
@@ -8,16 +8,34 @@ typedef struct _TabuItem
 	unsigned int source;
 	//!Swap : one of the two swapped elements ; EBSR / EFSR : the destination batch.
 	unsigned int destination;
+	//!The next element in the chained list.
+	struct _TabuItem * next;
 } TabuItem;
 
+//!Tabu list.
 typedef struct _TabuList{
-	TabuItem * item;
-	struct _TabuList * next;
+	//!First element of the list.
+	TabuItem * next;
+	//!Maximum size of the list.
 	unsigned int maxSize;
+	//!Current size of the list.
+	unsigned int size;
 } TabuList;
 
+/**
+ * Create a tabu list.
+ *
+ * @param maxSize The maximum size of the list.
+ * @return The list.
+ * @remark Needs to be freed with tabuList_destroy.
+ */
 TabuList * tabuList_create(unsigned int maxSize);
 
-void tabuLost_destroy();
+/**
+ * Frees a tabu list.
+ *
+ * @param list The list to destroy.
+*/
+void tabuList_destroy(TabuList * list);
 
 #endif //TABOU_TABULIST_H
